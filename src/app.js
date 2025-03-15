30beb7824f3a431f997ab816bd8b3729ddd90beb
ravimani1001 <inforavimani@gmail.com>
2025-03-15 23:09:19 +0530

    User and video model added

--- src/app.js
+++ src/app.js
@@ -13,14 +13,21 @@ app.use(cors( {
 } ))
 
 
-app.use(express.json({ limit : "16kb" }))
+
+//We will get data from many sources. Here we are configuring those sources
+
+app.use(express.json({ limit : "16kb" }))//configuring json with limit 16kb
+
 app.use(express.urlencoded({extended : true , limit : "16kb"}))
-//There wont be any problem if we dont give pass any object to the above json() and urlencoded()
+//this configuration is to accept data from different url encoding
+//There wont be any problem if we dont pass any object to the above json() and urlencoded()
 
 app.use(express.static("public"))
 //the above code is to access the static files that will stored in the public folder
 
 app.use(cookieParser())
+//cookie parser is used to perform CRUD operation on cookies on the user browser from the server
+//this helps to add and remove the secured cookies on the user browser 
 
 
 export { app }
